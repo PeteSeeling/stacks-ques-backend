167fadc7cd92617af67b1551577ce363f84f10d1
Pete Seeling <peteseeling@gmail.com>
2022-04-05 13:53:58 -0600

    stack and que method code written

---
+++ index.js
@@ -0,0 +1,70 @@
+
+class Stack{
+
+  constructor(value){
+    if (isNaN(value)){
+      value = 'gator';
+    }
+    this.value = value;
+    this.container = [];
+  }
+  push(value){
+    this.container.push(value);
+  }
+  pop(){
+    return this.container.length ? this.container.pop() : null;
+
+  }
+  peek(){
+    // return this.container.at(-1);
+  }
+}
+const stack = new Stack();
+stack.push('dog');
+stack.push('cat');
+stack.push('moose');
+console.log(stack.pop());
+stack.push('gazzelle');
+console.log(stack.pop());
+console.log(stack.peek());
+
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+  
+  // enqueue
+  enqueue(el) {
+    this.items.push(el);
+  }
+    
+  // dequeue
+  dequeue() {
+    this.items.shift();
+    return this.items;
+  }
+    
+  hasNext() {
+  
+    if (this.items) {
+      return true;
+    }
+  }
+  
+}
+
+const queue = new Queue();
+queue.enqueue('fox');
+queue.enqueue('goose');
+queue.enqueue('lizard');
+console.log(queue);
+console.log(queue.dequeue()); // 'fox'
+console.log(queue.hasNext()); // true
+console.log(queue.dequeue()); // 'goose'
+queue.enqueue('llama');
+console.log(queue.dequeue()); // 'lizard'
+console.log(queue.hasNext()); // true
+console.log(queue.dequeue()); // 'llama'
+console.log(queue.hasNext()); // false
+console.log(queue.dequeue()); // null
+
